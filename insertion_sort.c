6ab851bfe4ba072e0d2bff7a730abf6b982bb6a6
MertveZI <ss2013105@yandex.ru>
2025-10-13 17:39:59 +0700

    Coffe3 update
    Fixed:
    insertion_sort.c

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,46 +1,78 @@
 /*  Курс ОПК ФФ, 3 семестр, 2025г.
-    Задача № 6330 "Сортирока вставками"
+    Задача № 6330 "Сортировка вставками"
     Стерлягов Сергей, гр. 24310*/
 #include <stdio.h>
-#include <stdbool.h>
-#include <math.h>
 
+/** <p>Меняет элементы a и b местами.</p>
+  * 
+  * @param a указатель на элемент a
+  * @param b указатель на элемент b
+  */
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-/** <p>Сортируект массив методом вставок.</p>
+/** <p>Сортирует массив методом вставок.</p>
   * 
   * @param array массив, который необходимо отсортировать
+  * @param size размер массива
   */
-unsigned int generate_n_fib(unsigned int n) {
-    if (n == 1 || n == 2){
-        return 1;
-    }
-    else{ 
-        unsigned int f1 = 1;
-        unsigned int f2 = 1;
-        unsigned int i = 0;
-        unsigned int fib_sum = 0;
-        n = n - 2;
-        while(i < n){
-            fib_sum = f1 + f2;
-            f1 = f2;
-            f2 = fib_sum;
-            i = i + 1;
+void insertion_sort(int array[], int size) {
+    if (size > 1) {
+        for(int i = 1; i < size; i++) {
+            for(int j = i; j > 0 && array[j - 1] > array[j]; j--) {
+                swap(&array[j - 1], &array[j]); // Передаем указатели
+            }
         }
-        return f2;
     }
 }
 
-int main(void) {
-    unsigned int n = 0;
-    printf("Задача №6246 'Числа Фибоначчи'\n");
-    printf("Введите номер числа Фибоначчи(больше 0): ");
-    scanf_s("%u", &n);
-    unsigned int n_fib = generate_n_fib(n);
-    printf("Чисдло Фибоначчи под номером %u равно %u \n", n, n_fib);
-    
-    printf("\nНажмите Enter для выхода...");
-    getchar();
-    getchar();  
+/** <p>Выводит массив на экран.</p>
+  * 
+  * @param array массив для вывода
+  * @param size размер массива
+  */
+void print_array(int array[], int size) {
+    for(int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
 
+int main(void) {
+    unsigned int size = 0;
+    printf("Задача №6330 'Сортировка вставками'\n");
+    printf("Введите размер массива: ");
+    scanf_s("%d", &size);
+    
+    if (size <= 0) {
+        printf("Ошибка: размер массива должен быть положительным числом.\n");
+        printf("Нажмите Enter для выхода...");
+        getchar();
+        getchar();
+        return 1;
+    }
+    
+    int array[size];
+    
+    printf("Введите %d чисел через пробел: ", size);
+    for(int i = 0; i < size; i++) {
+        scanf_s("%d", &array[i]);
+    }
+    
+    printf("Исходный массив: ");
+    print_array(array, size);
+    
+    insertion_sort(array, size);
+    
+    printf("Отсортированный массив: ");
+    print_array(array, size);
+
+    printf("Нажмите Enter для выхода...");
+    getchar();
+    getchar();
+    
     return 0;
 }
